c0c718ba029881a6155967e3e8fc9382f26caf52
Panda17TK <bantian.tech@gmail.com>
2023-09-03 22:51:08 +0900

    add pydeck

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# 地図のサイズ
-width, height = 100, 100
-
-# 地図模擬データの作成
-map_image = np.ones((height, width, 3))
-
-# 3つの人流データを生成
-data1 = np.random.rand(height, width)
-data2 = np.random.rand(height, width) * 0.7  # データ2
-data3 = np.random.rand(height, width) * 1.3  # データ3
+import pydeck as pdk
 
 st.title("人流ヒートマップ表示")
 
+# 3つダミー人流データを生成
+data1 = np.random.rand(100, 100)
+data2 = np.random.rand(100, 100) * 0.7
+data3 = np.random.rand(100, 100) * 1.3
+
+# タイトル
+st.title("インタラクティブな人流ヒートマップ")
+
+# サイドバーにデータ選択用のセレクトボックスを追加
 selected_data = st.sidebar.selectbox(
     "表示するデータを選択してください",
     ("データ1", "データ2", "データ3")
 )
 
+# 選択されたデータに基づいてヒートマップデータを設定
 if selected_data == "データ1":
     heatmap_data = data1
 elif selected_data == "データ2":
@@ -28,15 +26,21 @@ elif selected_data == "データ2":
 else:
     heatmap_data = data3
 
-# ヒートマップの作成
-fig, ax = plt.subplots(figsize=(10, 10))
-ax.imshow(map_image, extent=[0, width, 0, height])
-sns.heatmap(heatmap_data, cmap='YlGnBu', alpha=0.7, cbar=True, ax=ax)
+# ヒートマップのデータをpydeck用の形式に変換
+heatmap_list = []
+for i in range(100):
+    for j in range(100):
+        heatmap_list.append([j, i, heatmap_data[i][j]])
 
-# カーソルをかざした位置の値を表示
-x, y = st.mouse_coordinates()
-if x and y:
-    st.write(f"位置: ({x:.2f}, {y:.2f}), 値: {heatmap_data[int(y), int(x)]:.2f}")
+# pydeckでヒートマップを作成
+layer = pdk.Layer(
+    "HeatmapLayer",
+    heatmap_list,
+    opacity=0.7,
+    get_position="[x, y]",
+    get_weight="z"
+)
 
-# Streamlitにヒートマップを表示
-st.pyplot(fig)
+view_state = pdk.ViewState(latitude=50, longitude=50, zoom=10)
+deck = pdk.Deck(layers=[layer], initial_view_state=view_state)
+st.pydeck_chart(deck)
